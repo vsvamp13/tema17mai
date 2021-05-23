0e1d0529b507d18b5342463e367e9b71102ce879
Vasile Drozdovschi <vaseav57@gmail.com>
2021-05-23 20:33:52 +0300

    completare

--- tema17mai.js
+++ tema17mai.js
@@ -69,4 +69,76 @@ switch (animal) {
 }
 
 //cicluri
-a
+
+for (let i = 1; i<=9; i++) {
+    console.log(i ** 2);
+}
+
+let e = 0, f = 0;
+    console.log('e = ', e++); //mai intii afiseaza valoare curenta 0, apoi aduna
+    console.log('f = ', ++f); //afiseaza rezultatul deja adunat cu 1
+
+//while
+
+let m = 0;
+while (m <= 9) {
+    console.log(m ** 2);
+    m++;
+}
+
+// do...while mai intii executa conditia apoi verifica
+// let m = 0;
+// do {
+//     console.log(m ** 2);
+//     m++;
+// } while (m <= 9);
+
+//break inside for
+for (let g = 0; g < 10; g++) {
+    if (g === 3) { break; }
+    console.log(g);
+    }
+//continue (se verifica pina la j valoarea 3, 3 se ignora, apoi se face skip si continua verificarea)
+for (let j = 0; j < 10; j++) {
+    if (j === 3) { continue; }
+    console.log(j);
+    }
+
+//functii
+function showName () {
+    console.log('Vasile Brinza');
+}
+showName(); //chemam functia ori de cate ori o scriem
+
+//vizibilitatea (aceasta este locala, daca o sa o chemam prin console.log(name), nu va aparea)
+function showNume () {
+    let name = 'Vasile Brinza';
+    console.log(name);
+}
+showNume();
+
+//variabila globala (aceasta poate fi citita de o variabila locala si afisata in consola, avem ani de 2 ori)
+let ani = 31;
+function showNome () {
+    let name = 'Vasile Brinza';
+    console.log(name);
+    console.log(ani);
+}
+console.log(ani);
+showNome();
+console.log(ani);
+
+//parametrii functiei (unei functii ii putem atribui un parametru)
+function showBug (bug, nr, end =' Moldova') {
+        console.log(bug, nr + end);
+}
+showBug('Buburuza', 20,);
+
+//return
+function fructeBune () {
+    console.log('mere');
+    return 'dulci'
+    //orice e scris dupa return nu se executa
+}
+let fructe = fructeBune();
+console.log(fructe);
